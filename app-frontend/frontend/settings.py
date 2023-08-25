2306f972a4625d1219b2dc8e0be74348d59288e9
stagoverflow <stagoverflow@gmail.com>
2023-08-25 15:51:25 -0400

    typo

--- app-frontend/frontend/settings.py
+++ app-frontend/frontend/settings.py
@@ -2,4 +2,4 @@ from yarl import URL
 
 #TODO: Duplicate..
 TITLE = "Energy Consumption Forecasting"
-API_URL = URL("http:///api:8001/api/v1")
+API_URL = URL("http://api:8001/api/v1")
--- app-monitoring/monitoring/settings.py
+++ app-monitoring/monitoring/settings.py
@@ -2,4 +2,4 @@ from yarl import URL
 
 #TODO: Duplicate..
 TITLE = "Monitoring | Energy Consumption"
-API_URL = URL("http:///api:8001/api/v1")
+API_URL = URL("http://api:8001/api/v1")
